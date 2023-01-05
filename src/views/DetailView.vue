d0c7426d553b81fefbe0cffe9c4b1edf1fb1750b
martineauAn <martineau.antoine2001@gmail.com>
2023-01-05 15:43:47 +0100

    commit before pull

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
+  <div
+      class="flex flex-grow p-5 w-full padding overflow-auto"
+    >
+  <div class="h-full w-full grid grid-cols-2">
+    <div class="h-full w-full">
+      <img class="object-fit" v-bind:src="detail.image_path" v-bind:alt="detail.permalink">
+    </div>
+    <div class="h-full w-full">
 
+    </div>
+  </div>
   </div>
 </template>
 
@@ -21,7 +30,8 @@
         fetchTvShow: async function() {
             try {
               const response = await axios.get("https://www.episodate.com/api/show-details?q=" + this.$route.params.title);
-              this.detail = this.detail.concat(response.data.tvShow);
+              this.detail = this.detail.concat(response.data.tvShow)[0];
+              //console.log(this.detail)
             } catch (err) {
               console.warn(err);
             }
